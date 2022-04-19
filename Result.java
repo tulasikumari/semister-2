9ca8ca0119dd94581b548ababc4aa633116fd661
unknown <kumaritulasi97@gmail.com>
2022-04-19 09:14:00 +0545

    try

--- Result.java
+++ Result.java
@@ -7,24 +7,32 @@
 //        Sub4 = 90;
 //        TotalMarks = (Sub1 + Sub2 + Sub3 + Sub4) / 4;
 //        System.out.println(TotalMarks);
-//        System.out.println(TotalMarks);
+//
+//        String grade;
+//        grade =(TotalMarks >=70? "First Class":
+//                (TotalMarks>=59 && TotalMarks<70)? "Upper Class":
+//                        (TotalMarks>=49 && TotalMarks<59)?"Second Class":
+//                                (TotalMarks>=39 && TotalMarks <49)? "Third Class":
+//                                        "fail");
+//        System.out.println(grade);
 //    }
 //}
 
-import javax.swing.*;
+//import javax.swing.*;
+//
+//public class Result {
+//    public static void main(String[] args) {
+//        double eng = Double.parseDouble(JOptionPane.showInputDialog("Enter the marks of math"));
+//        double nep = Double.parseDouble(JOptionPane.showInputDialog("Enter the marks of nep"));
+//        double social = Double.parseDouble(JOptionPane.showInputDialog("Enter the marks of social"));
+//        double eph = Double.parseDouble(JOptionPane.showInputDialog("Enter the marks of eph"));
+//        double total, percentage;
+//        total = eng + nep + social + eph;
+//        percentage = (total / 4);
+//        int castedpercentage = (int) (percentage);
+//        JOptionPane.showMessageDialog(null, castedpercentage + "%");
+//
+//    }
+//}
 
-public class Result {
-    public static void main(String[] args) {
-        double eng = Double.parseDouble(JOptionPane.showInputDialog("Enter the marks of math"));
-        double nep = Double.parseDouble(JOptionPane.showInputDialog("Enter the marks of nep"));
-        double social = Double.parseDouble(JOptionPane.showInputDialog("Enter the marks of social"));
-        double eph = Double.parseDouble(JOptionPane.showInputDialog("Enter the marks of eph"));
-        double total, percentage;
-        total = eng + nep + social + eph;
-        percentage = (total / 4);
-        int castedpercentage = (int) (percentage);
-        JOptionPane.showMessageDialog(null, castedpercentage + "%");
-
-    }
-}
 
